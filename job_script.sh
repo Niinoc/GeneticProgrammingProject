92c6a900a8fef0ffe49b2d1981471b997bf5b811
Niinoc <nino.kauder@gmail.com>
2024-06-27 16:45:21 +0200

    new correct shell scripts for Sun Grid Engine

--- job_script.sh
+++ job_script.sh
@@ -1,23 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 #$ -cwd
 #$ -o /dev/null
 #$ -e /dev/null
 
-# Lade die übergebenen Parameter
-param1=$param1
-param2=$param2
-param3=$param3
-param4=$param4
-param5=$param5
-param6=$param6
-param7=$param7
-param8=$param8
-input_file_name=$input_file_name
-run_dir=$run_dir
+cd /home/stud/other/pi96dal/GeneticProgrammingProject/
 
 # Java-Programm ausführen
-echo "Starting Java program for test $param1 with parameters: $param1 $param2 $param3 $param4 $param5 $param6 $param7 $param8"
-
 java -cp out com.mycompany.geneticprogramming.GeneticProgramming $param1 $param2 $param3 $param4 $param5 $param6 $param7 $param8 $input_file_name
 
 # kopiere Ausgabedateien aus log-Ordner in aktuellen Durchlauf-Ordner
@@ -25,12 +13,10 @@ cp log/out.bestfitness.txt "$run_dir/out.bestfitness.txt"
 cp log/out.finalbehavior.txt "$run_dir/out.finalbehavior.txt"
 cp log/out.finalpopulation.txt "$run_dir/out.finalpopulation.txt"
 cp log/out.finalprogram.txt "$run_dir/out.finalprogram.txt"
-cp log/fitnessFunction.txt "$run_dir/fitnessFunction.txt"
-cp log/initialbehavior.txt "$run_dir/initialbehavior.txt"
-cp log/initialpopulation.txt "$run_dir/initialpopulation.txt"
-cp log/parameters.txt "$run_dir/parameters.txt"
+cp log/out.fitnessFunction.txt "$run_dir/out.fitnessFunction.txt"
+cp log/out.initialbehavior.txt "$run_dir/out.initialbehavior.txt"
+cp log/out.initialpopulation.txt "$run_dir/out.initialpopulation.txt"
+cp log/out.parameters.txt "$run_dir/out.parameters.txt"
 
 # kopiere Eingabe-Datei in aktuellen Durchlauf-Ordner
 cp "in/in.$input_file_name.txt" "$run_dir/in.$input_file_name.txt"
-
-echo "Job completed."
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,42 +1,51 @@
-#!/bin/bash
+#!/bin/sh
+
+# setze Pfad
+cd /home/stud/other/pi96dal/GeneticProgrammingProject/
+
+# kompilieren
+javac -d out -sourcepath src/main/java src/main/java/com/mycompany/geneticprogramming/*.java
 
 # Anzahl der Durchläufe pro Test
-NUM_RUNS=3
+NUM_RUNS=2
 
 # Anzahl der Tests
-NUM_TESTS=3
+NUM_TESTS=5
 
 # Manuell festgelegte Parameter
 param2=5
 param3=15
-param5=1000
+param5=5000
 param6=0.1
 param7=0.3
-param8=0.1
+param8=0.2
 
 input_file_name=I.11.19
 
 # Hauptordner für die Tests
-test_dir=test_population_size
+test_dir=test_population_size2
 
 # erstelle Verzeichnis
-mkdir -p $test_dir
+mkdir -p "$test_dir"
+
+# erzuege Git Version txt in test_dir
+git -C /home/stud/other/pi96dal/GeneticProgrammingProject/ rev-parse HEAD > "$test_dir/git_version.txt"
 
 # Schleife für Tests
-for ((k=1; k<=NUM_TESTS; k++)); do
+for k in $(seq 1 $NUM_TESTS); do
     param4=$((10 + (k * 20)))
 
     # erstelle Ordner für jeden Testlauf (run_k)
-    test_run_dir=$test_dir/run_$k
+    test_run_dir="$test_dir/run_$k"
     mkdir -p "$test_run_dir"
 
     # Innere Schleife für Durchläufe
-    for ((i=1; i<=NUM_RUNS; i++)); do
-	# ändere nur Seed
+    for i in $(seq 1 $NUM_RUNS); do
+        # ändere nur Seed
         param1=$i
 
         # erstelle Ordner für jeden spezifischen Durchlauf (run_k_i)
-        run_dir=$test_run_dir/run_${k}_${i}
+        run_dir="$test_run_dir/run_${k}_${i}"
         mkdir -p "$run_dir"
 
         # übergebe Job an SGE
@@ -45,3 +54,4 @@ for ((k=1; k<=NUM_TESTS; k++)); do
 done
 
 echo "All jobs submitted."
+
