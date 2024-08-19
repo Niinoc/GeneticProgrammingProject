d1e2990f9901c03fdd8bce60765b3cfd2e22b79a
Niinoc <nicholas.benjamin.kauder@uni-jena.de>
2024-08-19 14:20:00 +0200

    Update main: Added seed selection
    - Added `pick_seeds` function to allow random selection of seeds for analysis.
    - Updated `plot_dir_name` to reflect new analysis configuration.
    - Configured main script to handle specific seeds and parameters.
    - Commented out additional seed and parameter settings for flexible testing.

--- test_auswertung/main.py
+++ test_auswertung/main.py
@@ -1,25 +1,37 @@
 import os
+import random
 
 from FitnessDataAnalyzerOverhaul import FitnessDataAnalyzerOverhaul
 
 
 def main():
-
-    base_dir = os.path.join('..', 'log', 'tests_with_old_operators', 'test_mutation_rate_very_fine')
+    base_dir = os.path.join('..', 'log', 'test_mutation_rate_new_analyzer_test')
     # Name des Ordners in den gespeichert werden soll
-    plot_dir_name = "test"
+    plot_dir_name = "auswertung_alles"
     # Funktionen welche berücksichtigt werden sollen, wenn None -> alle
-    functions = ['I.8.14', 'I.11.19', 'I.27.6', 'I.29.4', 'I.39.1', 'I.50.26', 'II.3.24', 'II.11.28', 'II.38.14']
+    functions = None
+    # ['I.8.14', 'I.11.19', 'I.27.6', 'I.29.4', 'I.39.1', 'I.50.26', 'II.3.24', 'II.11.28', 'II.38.14']
     # Parameterwerte welche berücksichtigt werden sollen, wenn None -> alle
     parameters = None
-    
-    analyzer = FitnessDataAnalyzerOverhaul(base_dir, plot_dir_name, functions, parameters)
-    
+    # seeds = pick_seeds(10)
+    # seeds = ['seed_1', 'seed_2', 'seed_3']
+    seeds = None
+    # print(seeds)
+    analyzer = FitnessDataAnalyzerOverhaul(base_dir, plot_dir_name, functions, parameters, seeds)
+
     print(analyzer.df)
     print(analyzer.average_df)
 
     analyzer.save_all_test_data()
     analyzer.save_all_plots()
+
+    # nur mit 2 parametern und sehr wenigen runs oder mit einem parameter verwenden
+    # -> zB functions=None, parameters=['0.09'], seeds=['seed_1','seed_2','seed_3','seed_4','seed_5']
+    # analyzer.save_individual_fitness_plot()
+
+
+def pick_seeds(anzahl):
+    return [f'seed_{num}' for num in random.sample(range(1, 50 + 1), anzahl)]
 
 
 if __name__ == '__main__':
