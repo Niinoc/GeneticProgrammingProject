cbcd3889fe988127aa34a117ccae9083b58bdccc
Niinoc <nino.kauder@gmail.com>
2024-08-28 11:43:38 +0200

    changed javac use: --release 11

--- submit_jobs_mut.sh
+++ submit_jobs_mut.sh
@@ -5,10 +5,10 @@
 cd /home/stud/other/pi96dal/GeneticProgrammingProject/
 
 # kompilieren
-# javac -source 17 -target 11 -d out -sourcepath src/main/java src/main/java/com/mycompany/geneticprogramming/*.java
+javac --release 11 -d out -sourcepath src/main/java src/main/java/com/mycompany/geneticprogramming/*.java
 
 # Anzahl der Durchläufe pro Test
-NUM_RUNS=50
+NUM_RUNS=20
 
 # Manuell festgelegte Parameter
 numOfInputs=5
@@ -16,17 +16,17 @@ numOfInstructions=15
 populationSize=250
 numOfGenerations=25000
 # Liste der mutationRate Werte
-mutationRate_list="0.01 0.03 0.05 0.07 0.09 0.15 0.2 0.25 0.3 0.35"
+mutationRate_list="0.07 0.09 0.15"
 mutationRateRegisters=0.3
 mutationStrengthRegisters=0.2
 
-input_file_list="I.8.14 I.11.19 I.27.6 I.29.4 I.39.1 I.50.26 II.3.24 II.11.28 II.38.14"
+input_file_list="I.6.2a I.8.14 I.11.19 II.11.28 III.9.52 I.27.6 I.39.1 I.50.26 II.3.24 II.6.15a"
 
 # erstelle Verzeichnis
 log_dir=log
 mkdir -p $log_dir
 
-test_dir="$log_dir/test_mutation_rate"
+test_dir="$log_dir/test_VersionC_double_in_regs"
 mkdir -p $test_dir
 
 # erzeuge Git Version txt in log
