49ddd5bae2d12096bc252a582348a3444db15dd6
Niinoc <nicholas.benjamin.kauder@uni-jena.de>
2024-08-29 14:53:11 +0200

    Refactor Operator class: Remove function lambdas and streamline operators to focus on names and operand counts
    - Removed all lambda functions from the Operator enum as calculation logic has been moved to the Interpreter.
    - Simplified the Operator constructor by eliminating the now-unused Function<Double[], Double> parameter.
    - Adjusted the enum to only store operator names and operand counts, reducing memory usage and improving clarity.
    - Removed unused constant operators that are no longer required.

--- src/main/java/com/mycompany/geneticprogramming/Operator.java
+++ src/main/java/com/mycompany/geneticprogramming/Operator.java
@@ -15,59 +15,43 @@ import java.util.function.Function;
  * @author Peter & Nicholas
  */
 public enum Operator {
-    /*
-    ADD("+", input -> input[0] + input[1], 3),
-    SUB("-", input -> input[0] - input[1], 3),
-    MUL("*", input -> input[0] * input[1], 3),
-    DIV("/", input -> input[1] != 0 ? input[0] / input[1] : 0, 3), // Geschützte Division
 
-    NEG("-", input -> -(input[0]), 2),
-    INV("1/", input -> input[0] != 0 ? 1 / input[0] : 0, 2), // Geschützte Inversion
+    ADD("+", 3),
+    SUB("-", 3),
+    MUL("*", 3),
+    DIV("/", 3),
+    NEG("-", 2),
+    INV("1/", 2),
+    SQRT("sqrt", 2),
+    EXP("**", 3),
+    LN("ln", 2),
+    SIN("sin", 2),
+    COS("cos", 2),
+    TAN("tan", 2);
 
-    SQRT("sqrt", input -> input[0] >= 0 ? Math.sqrt(input[0]) : 0, 2), // Geschützte Quadratwurzel
-    EXP("**", input -> {
-        if (input[0] == 0) return (input[1] > 0) ? 0 : Math.pow(10.0, 8.0);  // 0^positive = 0, 0^negative = "Infinity"
+    final String name;
+    final int numberOfOperands;
 
-        else if (input[0] < 0 && input[1] % 1 != 0) return 0.0;  // negative base and non-integer exponent result in NaN, handle as 0
+    Operator(String name, int numberOfOperands) {
+        this.name = name;
+        this.numberOfOperands = numberOfOperands;
+    }
 
-        else return Math.pow(input[0], input[1]);
-    }, 3),
-    LN("ln", input -> input[0] > 0 ? Math.log(input[0]) : 0, 2), // Geschützter Logarithmus (nur für positive Werte)
+    /***
+     * Returns a randomly selected operator.
+     * @return A random operator.
+     */
+    public static Operator random() {
+        return Operator.values()[MyRandom.nextInt(Operator.values().length)];
+    }
 
-    SIN("sin", input -> Math.sin(input[0]), 2),
-//    ARCSIN("arcsin", input -> Math.asin(input[0]), 2),
-    COS("cos", input -> Math.cos(input[0]), 2),
-//    ARCCOS("arccos", input -> Math.acos(input[0]), 2),
-    TAN("tan", input -> Math.tan(input[0]), 2),
-//    ARCTAN("arctan", input -> Math.atan(input[0]), 2),
+    @Override
+    public String toString() {
+        return name;
+    }
+}
 
-    PI("3.141592*", input -> Math.PI*(input[0]), 2),
-    EUL("e^", input -> Math.exp(input[0]), 2),
-
-    //mehr mit konstanten
-    ;
-    */
-    
-    ADD("+", input -> input[0] + input[1], 3),
-    SUB("-", input -> input[0] - input[1], 3),
-    MUL("*", input -> input[0] * input[1], 3),
-    DIV("/", input -> input[0] / input[1], 3),
-
-    NEG("-", input -> -(input[0]), 2),
-    INV("1/", input -> 1 / input[0], 2),
-
-    SQRT("sqrt", input -> Math.sqrt(input[0]), 2),
-    EXP("**", input -> Math.pow(input[0], input[1]), 3),
-    LN("ln", input ->  Math.log(input[0]), 2),
-
-    SIN("sin", input -> Math.sin(input[0]), 2),
-    //    ARCSIN("arcsin", input -> Math.asin(input[0]), 2),
-    COS("cos", input -> Math.cos(input[0]), 2),
-    //    ARCCOS("arccos", input -> Math.acos(input[0]), 2),
-    TAN("tan", input -> Math.tan(input[0]), 2),
-    //    ARCTAN("arctan", input -> Math.atan(input[0]), 2),
-
-    /*PI("3.141592*", input -> Math.PI * input[0], 2),
+/*PI("3.141592*", input -> Math.PI * input[0], 2),
     EUL("e*", input -> Math.E * input[0], 2),
     HALF("0.5*", input -> 0.5 * input[0], 2),
     negHALF("-0.5*", input -> 0.5 * input[0], 2),
@@ -78,30 +62,4 @@ public enum Operator {
     THREE("3*", input -> 3.0 * input[0], 2),
     negTHREE("-3*", input -> 3.0 * input[0], 2),*/
 
-    //mehr mit konstanten
-    ;
-
-    final String name;
-    final Function<Double [],Double> function;
-    final int numberOfOperands;
-
-    Operator (String name, Function<Double [], Double> function, int numberOfOperands) {
-        this.name = name;
-        this.function = function;
-        this.numberOfOperands = numberOfOperands;
-    }
-
-    /***
-     * Returns a randomly selected operator.
-     * @return A random operator.
-     */
-    public static Operator random(){
-        return Operator.values()[MyRandom.nextInt(Operator.values().length)];
-    }
-
-    @Override
-        public String toString(){
-            return name;
-        }
-
-}
+//mehr mit konstanten
