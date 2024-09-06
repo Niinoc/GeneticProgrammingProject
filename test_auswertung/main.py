b4fb5271dc31b49da26f491cbaacf87f1464600f
Niinoc <nicholas.benjamin.kauder@uni-jena.de>
2024-09-06 18:12:34 +0200

    prepared main to handle new analyzer

--- test_auswertung/main.py
+++ test_auswertung/main.py
@@ -3,10 +3,18 @@ import random
 
 from FitnessDataAnalyzerOverhaul import FitnessDataAnalyzerOverhaul
 from RandomSearchAnalyzer import RandomSearchAnalyzer
+from ProgressManager import ProgressManager
 
 
 def main():
-    base_dir = os.path.join('..', 'log', 'test_v4')
+    base_dir = os.path.join('..', 'log', 'test_randomsearchV4')
+
+    tested_file = '/path/to/tested_combinations.json'
+    processed_file = '/path/to/processed_combinations.json'
+
+    progress_manager = ProgressManager(tested_file, processed_file)
+    unprocessed_combinations = progress_manager.get_unprocessed_combinations(batch_size=100)
+
     # Name des Ordners in den gespeichert werden soll
     plot_dir_name = "auswertung_alles"
     # Funktionen welche berücksichtigt werden sollen, wenn None -> alle
@@ -14,7 +22,7 @@ def main():
 
     # ['I.8.14', 'I.11.19', 'I.27.6', 'I.29.4', 'I.39.1', 'I.50.26', 'II.3.24', 'II.11.28', 'II.38.14']
     # Parameterwerte welche berücksichtigt werden sollen, wenn None -> alle
-    parameters = None
+    # parameters = None
     # # seeds = pick_seeds(10)
     # # seeds = ['seed_1', 'seed_2', 'seed_3']
     # seeds = None
@@ -30,10 +38,12 @@ def main():
     # -> zB functions=None, parameters=['0.09'], seeds=['seed_1','seed_2','seed_3','seed_4','seed_5']
     # analyzer.save_individual_fitness_plot()
 
-    rs_analyzer = RandomSearchAnalyzer(base_dir, functions)
+    rs_analyzer = RandomSearchAnalyzer(base_dir, unprocessed_combinations, functions)
     print(rs_analyzer.df)
     print(rs_analyzer.average_df)
     rs_analyzer.save_test_data()
+
+    progress_manager.mark_as_processed(unprocessed_combinations)
 
 
 def pick_seeds(anzahl):
